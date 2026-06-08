9306a91528eb202c26b08b78540b012a1fd9dfdc
Filip Hanek <filiphanek22@gmail.com>
2026-06-09 00:03:56 +0200

    fix: poprawa wydajnosci TicketController oraz integracja Spring Security z baza

--- src/main/java/com/eventsystem/event_management/config/SecurityConfig.java
+++ src/main/java/com/eventsystem/event_management/config/SecurityConfig.java
@@ -5,15 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 
@@ -24,6 +20,11 @@ import org.springframework.context.annotation.Configuration;
 )
 
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -45,19 +46,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user@event.pl")
-                .password("{noop}user123")
-                .roles("USER")
-                .build();
-
-        String adminUsername = "admin@event.pl";
-        UserDetails admin = User.withUsername(adminUsername)
-                .password("{noop}admin123")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
 }
